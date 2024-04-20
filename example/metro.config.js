418acf678f4c7bea209941e83f3b6575b8477135
abretonc7s <arthur.breton@consensys.net>
2024-04-20 14:55:28 +0800

    feat: setup

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,6 +1,6 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
+const path = require('node:path');
 
 const config = getDefaultConfig(__dirname);
 
