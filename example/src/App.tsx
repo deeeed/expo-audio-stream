139aac26a4b453c0f908dc7e64a5d9953c6290b9
abretonc7s <arthur.breton@consensys.net>
2024-04-22 18:15:55 +0800

    feat: setup

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,16 +1,27 @@
-import { Audio } from 'expo-av';
-import { Button, Platform, ScrollView, StyleSheet, Text, View } from 'react-native';
+import {
+  clearAudioFiles,
+  listAudioFiles,
+  useAudioRecorder,
+} from "expo-audio-stream";
+import { Audio } from "expo-av";
+import { useCallback, useRef, useState } from "react";
+import {
+  Button,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
-import { clearAudioFiles, listAudioFiles, useAudioRecorder } from 'expo-audio-stream';
-import { useCallback, useRef, useState } from 'react';
-import { AudioStreamResult } from '../../src/ExpoAudioStream.types';
-import { formatBytes, formatDuration } from './utils';
+import { formatBytes, formatDuration } from "./utils";
+import { AudioStreamResult } from "../../src/ExpoAudioStream.types";
 
-let audioChunks: Blob[] = [];
-const isWeb = Platform.OS === 'web';
+const audioChunks: Blob[] = [];
+const isWeb = Platform.OS === "web";
 
 if (isWeb) {
-  localStorage.debug = 'expo-audio-stream:*';
+  localStorage.debug = "expo-audio-stream:*";
 }
 
 export default function App() {
@@ -25,42 +36,41 @@ export default function App() {
     console.log(`audio event ${typeof audioData}`, audioData);
     // Append the audio data to the audioRef
     audioChunks.push(audioData);
-  }
+  };
 
-  const { startRecording, stopRecording, duration, size, isRecording } = useAudioRecorder({ onAudioStream: onAudioData });
-
+  const { startRecording, stopRecording, duration, size, isRecording } =
+    useAudioRecorder({ onAudioStream: onAudioData });
 
   const handleStart = async () => {
     const { granted } = await Audio.requestPermissionsAsync();
     if (!granted) {
-      setError("Permission not granted!")
+      setError("Permission not granted!");
     }
     const url = await startRecording({ interval: 500 });
     console.debug(`Recording started at ${url}`);
-    if (url)
-      setAudioUri(url);
-  }
+    if (url) setAudioUri(url);
+  };
 
   const handleStopRecording = useCallback(async () => {
     if (!isRecording) return;
     const result = await stopRecording();
     console.debug(`Recording stopped. `, result);
     setResult(result);
-  }, [isRecording])
+  }, [isRecording]);
 
   const handleListFiles = async () => {
     const _files = await listAudioFiles();
     setFiles(_files);
-  }
+  };
 
   const handleClearStorage = async () => {
     try {
-      await clearAudioFiles()
-      await handleListFiles()
+      await clearAudioFiles();
+      await handleListFiles();
     } catch (error) {
-      console.error(`Error while clearing storage`, error)
+      console.error(`Error while clearing storage`, error);
     }
-  }
+  };
 
   const renderRecording = () => (
     <View>
@@ -77,19 +87,17 @@ export default function App() {
   );
 
   if (error) {
-    return <View>
-      <Text>{error}</Text>
-      <Button onPress={() => handleStart} title='Try Again' />
-    </View>
+    return (
+      <View>
+        <Text>{error}</Text>
+        <Button onPress={() => handleStart} title="Try Again" />
+      </View>
+    );
   }
 
   const renderRecordings = () => (
-    <View>
-      {files?.map((file, index) => (
-        <Text key={index}>{file}</Text>
-      ))}
-    </View>
-  )
+    <View>{files?.map((file, index) => <Text key={index}>{file}</Text>)}</View>
+  );
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={{ gap: 10 }}>
@@ -97,27 +105,34 @@ export default function App() {
         <Button title="Find existing Recordings" onPress={handleListFiles} />
         <Button title="Clear Storage" onPress={handleClearStorage} />
       </View>
-      {audioUri && <View>
-        <Text>Audio URI: {audioUri}</Text>
-      </View>}
-      {result && <View>
-        <Text>{JSON.stringify(result, null, 2)}</Text>
-        <Button title="Play Recording" onPress={async () => {
-          try {
-            let url = result.fileUri;
-            if (isWeb) {
-              const blob = new Blob(audioChunks, { type: 'audio/webm' });
-              url = URL.createObjectURL(blob);
-            }
-            const sound = new Audio.Sound();
-            console.log(`playing file uri ${result.fileUri}`, url)
-            await sound.loadAsync({ uri: url });
-            await sound.playAsync();
-          } catch (error) {
-            console.error(`error playing audio`, error)
-          }
-        }} />
-      </View>}
+      {audioUri && (
+        <View>
+          <Text>Audio URI: {audioUri}</Text>
+        </View>
+      )}
+      {result && (
+        <View>
+          <Text>{JSON.stringify(result, null, 2)}</Text>
+          <Button
+            title="Play Recording"
+            onPress={async () => {
+              try {
+                let url = result.fileUri;
+                if (isWeb) {
+                  const blob = new Blob(audioChunks, { type: "audio/webm" });
+                  url = URL.createObjectURL(blob);
+                }
+                const sound = new Audio.Sound();
+                console.log(`playing file uri ${result.fileUri}`, url);
+                await sound.loadAsync({ uri: url });
+                await sound.playAsync();
+              } catch (error) {
+                console.error(`error playing audio`, error);
+              }
+            }}
+          />
+        </View>
+      )}
       {isRecording && renderRecording()}
       {!isRecording && renderStopped()}
       {renderRecordings()}
@@ -129,8 +144,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     gap: 10,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
