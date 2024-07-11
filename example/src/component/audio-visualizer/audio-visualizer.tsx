33095feffdfc85ffa7b12d041851afb2267b16d4
abretonc7s <arthur.breton@consensys.net>
2024-07-11 11:23:48 +0800

    fix: incorrect types

--- example/src/component/audio-visualizer/audio-visualizer.tsx
+++ example/src/component/audio-visualizer/audio-visualizer.tsx
@@ -132,7 +132,7 @@ export const AudioVisualizer: React.FC<AudioVisualizerProps> = ({
   const totalCandleWidth =
     audioData.dataPoints.length * (candleWidth + candleSpace);
 
-  const activePointsRef = useRef([]);
+  const activePointsRef = useRef<CandleData[]>([]);
 
   // Initialize activePoints
   useEffect(() => {
