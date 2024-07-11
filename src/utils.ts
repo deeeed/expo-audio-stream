b83534a4470beaf8b777bb67a8f4b516d1bf2cd9
abretonc7s <arthur.breton@consensys.net>
2024-07-11 18:12:35 +0800

    feat: manage files on web

--- src/utils.ts
+++ src/utils.ts
@@ -163,16 +163,6 @@ export const getWavFileInfo = async (
   };
 };
 
-// Helper method to generate a UUID
-export const quickUUID = () => {
-  // Implementation of UUID generation (use a library or custom method)
-  return "xxxx-xxxx-xxxx-xxxx".replace(/[x]/g, (c) => {
-    const r = (Math.random() * 16) | 0,
-      v = c === "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-};
-
 export const encodingToBitDepth = ({
   encoding,
 }: {
