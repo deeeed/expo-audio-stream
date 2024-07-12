885733079b09f3e9c5b2f0f887adce960bdac2ef
abretonc7s <arthur.breton@consensys.net>
2024-07-12 21:38:14 +0800

    feat: Add ApplicationContextProvider for debug mode in RootLayout

--- playground/src/app/_layout.tsx
+++ playground/src/app/_layout.tsx
@@ -5,18 +5,21 @@ import { Stack } from "expo-router/stack";
 
 import { AudioRecorderProvider } from "../../../src";
 import { AudioFilesProvider } from "../context/AudioFilesProvider";
+import { ApplicationContextProvider } from "../context/application-context";
 export default function RootLayout() {
   return (
     <LoggerProvider>
-      <AudioRecorderProvider config={{ debug: true }}>
-        <UIProvider>
-          <AudioFilesProvider>
-            <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            </Stack>
-          </AudioFilesProvider>
-        </UIProvider>
-      </AudioRecorderProvider>
+      <ApplicationContextProvider debugMode>
+        <AudioRecorderProvider config={{ debug: true }}>
+          <UIProvider>
+            <AudioFilesProvider>
+              <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              </Stack>
+            </AudioFilesProvider>
+          </UIProvider>
+        </AudioRecorderProvider>
+      </ApplicationContextProvider>
     </LoggerProvider>
   );
 }
