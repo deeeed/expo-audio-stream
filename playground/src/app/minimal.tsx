14c1697294dbc14b9e089a232d2ad2bb0c82a2a5
abretonc7s <arthur.breton@consensys.net>
2024-07-13 17:16:05 +0800

    feat: cleanup

--- playground/src/app/minimal.tsx
+++ playground/src/app/minimal.tsx
@@ -308,15 +308,22 @@ const Minimal = () => {
           >
             Toggle Mode (Current: {mode})
           </Button>
-          <Button
-            onPress={() => {
-              translateX.value = 0;
-              updateActivePoints(0);
-            }}
-          >
-            reset
-          </Button>
-          <Button onPress={addWavePoints}>Add Wavepoints</Button>
+          {mode === "live" ? (
+            <>
+              <Button onPress={addWavePoints}>Add Wavepoints</Button>
+            </>
+          ) : (
+            <>
+              <Button
+                onPress={() => {
+                  translateX.value = 0;
+                  updateActivePoints(0);
+                }}
+              >
+                reset
+              </Button>
+            </>
+          )}
         </View>
         <View>
           <Text>translareX: {translateX.value}</Text>
@@ -325,9 +332,6 @@ const Minimal = () => {
           <Text>canvasWidth: {canvasWidth}</Text>
           <Text>MaxDisplayedItems: {maxDisplayedItems}</Text>
           <Text>StartIndex: {startIndex}</Text>
-          <Text>
-            range: {min} , {max}
-          </Text>
         </View>
         <GestureDetector gesture={panGesture}>
           <View style={styles.canvasContainer}>
