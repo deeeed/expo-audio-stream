6784399533aecf37489213fc8742e33b5435687d
abretonc7s <arthur.breton@consensys.net>
2024-07-17 18:11:14 +0800

    feat: clean build minimal

---
+++ apps/minimal/app/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Test ok.</Text>
+    </View>
+  );
+}
