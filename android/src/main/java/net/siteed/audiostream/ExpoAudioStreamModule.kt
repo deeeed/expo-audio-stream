90f499fd2ea16986e5e40376aef764762a4cd5a4
Arthur <deeeed@users.noreply.github.com>
2024-07-17 09:10:10 +0800

    Implement refresh control and add clipboard dependency and babel asset mapping (#5)
    * feat: implement refresh control
    
    * feat: add clipboard dependency and babel asset mapping
    
    * feat: Add ApplicationContextProvider for debug mode in RootLayout
    
    * feat: Add LogViewer component for displaying logs in the playground app
    
    * feat: Add MaterialIcons to TabLayout for additional tab options
    
    * feat: add link to full audio recording settings
    
    * feat: Update FullAudioViewerPage to set navbar title and navigate to "/files" after deleting a file
    
    * feat: cleanup
    
    * feat: cleanup
    
    * refactor: Update FilesScreen component to use FlatList instead of ScrollView
    
    * refactor: Update CanvasContainer component to handle touch events and improve performance
    
    * chore: Update @siteed/design-system to version 0.8.10
    
    * feat: add data viewer component
    
    * feat: Add audio recording configuration form to FullAudioViewerPage
    
    * refactor: Update FullAudioViewerPage to use filename instead of fileUri for selected file
    
    * setup: clean build
    
    * feat: temp rename
    
    * feat: align asset files name
    
    * feat: ui improvements
    
    * feat: change analysis config
    
    * feat: align audio features ios web android

--- android/src/main/java/net/siteed/audiostream/ExpoAudioStreamModule.kt
+++ android/src/main/java/net/siteed/audiostream/ExpoAudioStreamModule.kt
@@ -47,7 +47,7 @@ class ExpoAudioStreamModule() : Module(), EventSender {
 
         AsyncFunction("extractAudioAnalysis") { options: Map<String, Any>, promise: Promise ->
             val fileUri = options["fileUri"] as? String
-            val pointsPerSecond = options["pointsPerSecond"] as? Int ?: 20
+            val pointsPerSecond =  (options["pointsPerSecond"] as? Double) ?: 20.0
             val algorithm = options["algorithm"] as? String ?: "rms"
             val featuresMap = options["features"] as? Map<*, *>
             val features = featuresMap?.filterKeys { it is String }
@@ -55,6 +55,7 @@ class ExpoAudioStreamModule() : Module(), EventSender {
                 ?.mapKeys { it.key as String }
                 ?.mapValues { it.value as Boolean }
                 ?: emptyMap()
+            val skipWavHeader = (options["skipWavHeader"] as? Boolean) ?: false
 
             if (fileUri == null) {
                 promise.reject("INVALID_ARGUMENTS", "fileUri is required", null)
@@ -62,7 +63,7 @@ class ExpoAudioStreamModule() : Module(), EventSender {
             }
 
             try {
-                val audioData = audioProcessor.loadAudioFile(fileUri)
+                val audioData = audioProcessor.loadAudioFile(fileUri, skipWavHeader)
                 if (audioData == null) {
                     promise.reject("PROCESSING_ERROR", "Failed to load audio file", null)
                     return@AsyncFunction
--- playground/src/app/_layout.tsx
+++ playground/src/app/_layout.tsx
@@ -1,22 +1,63 @@
 // playground/src/app/_layout.tsx
+import { MaterialIcons } from "@expo/vector-icons";
 import { UIProvider } from "@siteed/design-system";
 import { LoggerProvider } from "@siteed/react-native-logger";
+import { useRouter } from "expo-router";
 import { Stack } from "expo-router/stack";
 
 import { AudioRecorderProvider } from "../../../src";
 import { AudioFilesProvider } from "../context/AudioFilesProvider";
+import { ApplicationContextProvider } from "../context/application-context";
 export default function RootLayout() {
+  const router = useRouter();
+
   return (
     <LoggerProvider>
-      <AudioRecorderProvider config={{ debug: true }}>
-        <UIProvider>
-          <AudioFilesProvider>
-            <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            </Stack>
-          </AudioFilesProvider>
-        </UIProvider>
-      </AudioRecorderProvider>
+      <ApplicationContextProvider debugMode>
+        <AudioRecorderProvider config={{ debug: true }}>
+          <UIProvider
+            toastProviderProps={{
+              overrides: {
+                snackbarStyle: {
+                  marginBottom: 40,
+                },
+              },
+            }}
+          >
+            <AudioFilesProvider>
+              <Stack
+                screenOptions={{
+                  headerLeft: ({ label, canGoBack, tintColor }) => {
+                    if (canGoBack) {
+                      return (
+                        <MaterialIcons
+                          name="arrow-back-ios"
+                          size={24}
+                          color={tintColor}
+                          onPress={() => router.back()}
+                          style={{ paddingRight: 10, paddingLeft: 10 }}
+                        />
+                      );
+                    } else {
+                      return (
+                        <MaterialIcons
+                          name="home"
+                          size={24}
+                          color={tintColor}
+                          onPress={() => router.navigate("/")}
+                          style={{ paddingRight: 10, paddingLeft: 10 }}
+                        />
+                      );
+                    }
+                  },
+                }}
+              >
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              </Stack>
+            </AudioFilesProvider>
+          </UIProvider>
+        </AudioRecorderProvider>
+      </ApplicationContextProvider>
     </LoggerProvider>
   );
 }
