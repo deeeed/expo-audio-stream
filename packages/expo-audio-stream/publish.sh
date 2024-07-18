87b0d00cb85a8db89ec6ade9fd593d610133e942
Arthur <deeeed@users.noreply.github.com>
2024-07-18 09:05:20 +0800

    Move project to monorepo structure (#9)
    * feat: move playground to apps
    
    * feat: move to packages folder
    
    * feat: move to monorepo structure
    
    * feat: wip
    
    * feat: init minimal demo
    
    * feat: prebuild clean
    
    * feat: cleanup
    
    * feat: wip
    
    * feat: init ui project
    
    * feat: cleanup
    
    * feat: prebuild minimal
    
    * feat: clean build minimal
    
    * feat: cleanup
    
    * feat: setup minimal project
    
    * feat: prebuild
    
    * feat: wip
    
    * feat: bump version to $version
    
    * feat: wip
    
    * feat: minimal setup
    
    * Refactor ExpoAudioStreamModule to improve performance and avoid unnecessary object creation
    
    * feat: Improve performance and avoid unnecessary object creation in ExpoAudioStreamModule
    
    * chore: Update dependencies and remove unused imports
    
    * feat: cleanup
    
    * feat: Go to the newly saved page after recording
    
    * feat: highlight selected candle even when not displayed

---
+++ packages/expo-audio-stream/publish.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+# Bump version
+yarn version patch
+version=$(node -p "require('./package.json').version")
+git add .
+git commit -m 'feat: bump version to $version'
+yarn clean && yarn prepare && npm publish
