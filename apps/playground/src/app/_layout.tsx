df1ba5fd5700a307da42720ef604df4e59eb4005
abretonc7s <arthur.breton@consensys.net>
2024-12-04 20:40:40 +0800

    chore: update dependencies and fix reanimated web hack
    - Bump expo to version 52.0.14
    - Update expo-router to version 4.0.11
    - Update react-native-reanimated to version 3.17.0-nightly-20241204-5ffa47792
    - Update expo-modules-autolinking to version 2.0.3
    - Update expo-modules-core to version 2.1.1
    - Remove temporary worklet initialization patch for reanimated in index.web.tsx
    - Implement conditional reanimated hack in RootLayout based on URL parameters

--- apps/playground/src/app/_layout.tsx
+++ apps/playground/src/app/_layout.tsx
@@ -1,11 +1,13 @@
 // playground/src/app/_layout.tsx
-import { ThemeProvider } from '@react-navigation/native'
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { useTheme } from '@siteed/design-system'
 import { AudioRecorderProvider } from '@siteed/expo-audio-stream'
-import { enabled, getLogger } from '@siteed/react-native-logger'
+import { getLogger } from '@siteed/react-native-logger'
 import Constants from 'expo-constants'
+import { useGlobalSearchParams } from 'expo-router'
 import { Stack } from 'expo-router/stack'
 import { useEffect } from 'react'
+import { Platform } from 'react-native'
 
 import { config } from '../config'
 import { ApplicationContextProvider } from '../context/ApplicationProvider'
@@ -16,13 +18,34 @@ const logger = getLogger('RootLayout')
 export default function RootLayout() {
     const baseUrl = Constants.expoConfig?.experiments?.baseUrl ?? ''
     const theme = useTheme()
+    const { hack } = useGlobalSearchParams()
 
     useEffect(() => {
-        logger.debug(`Base URL: ${baseUrl}`)
-        console.log(`Base URL: ${baseUrl}`)
-        console.debug(`logger`, logger)
-        console.debug(`enabled()`, enabled('RootLayout'))
-    }, [baseUrl])
+        logger.log(`Base URL: ${baseUrl}`)
+
+        // FIXME: should be removed once react-native-reanimated is fixed https://github.com/software-mansion/react-native-reanimated/issues/6740
+        // Apply the reanimated hack based on URL parameter
+        if (Platform.OS === 'web') {
+            const shouldApplyHack = hack === 'true'
+            logger.log(`hack: ${hack} --> shouldApplyHack: ${shouldApplyHack}`)
+            if (shouldApplyHack) {
+                global._WORKLET = false
+                // @ts-expect-error
+                global._log = console.log
+                // @ts-expect-error
+                global._getAnimationTimestamp = () => performance.now()
+                logger.debug('Applied reanimated web hack')
+            } else {
+                // Remove the hack by deleting the global values
+                delete global._WORKLET
+                // @ts-expect-error
+                delete global._log
+                // @ts-expect-error
+                delete global._getAnimationTimestamp
+                logger.debug('Removed reanimated web hack')
+            }
+        }
+    }, [baseUrl, hack])
 
     return (
         <ApplicationContextProvider debugMode>
@@ -35,7 +58,12 @@ export default function RootLayout() {
                     }}
                 >
                     <AudioFilesProvider>
-                        <ThemeProvider value={theme}>
+                        <ThemeProvider
+                            value={{
+                                ...theme,
+                                fonts: DefaultTheme.fonts,
+                            }}
+                        >
                             <Stack
                                 screenOptions={{
                                     headerBackButtonMenuEnabled: false,
