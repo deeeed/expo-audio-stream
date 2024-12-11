4682aba187bfac224b3e9c00f3c9d1a5e127a764
abretonc7s <arthur.breton@consensys.net>
2024-12-11 13:33:57 +0800

    chore(expo-audio-stream): improved build publish script

--- packages/expo-audio-stream/publish.sh
+++ packages/expo-audio-stream/publish.sh
@@ -17,19 +17,27 @@ yarn clean
 yarn build
 yarn build:plugin
 
-# Update documentation
-echo -e "${YELLOW}Generating updated documentation...${NC}"
-yarn docgen
-
-echo -e "${BLUE}Waiting while docs are generated...${NC}"
-sleep 2
-
 echo -e "${YELLOW}Starting publication process...${NC}"
 publisher release
 
 # Get new version after bump
 version=$(node -p "require('./package.json').version")
 echo -e "${GREEN}New version: $version${NC}"
+
+# Ask about generating documentation (default Yes)
+read -p "$(echo -e ${YELLOW}Do you want to generate updated documentation? [Y/n]: ${NC})" generate_docs
+if [[ $generate_docs =~ ^[Nn]$ ]]; then
+    echo -e "${BLUE}Skipping documentation generation${NC}"
+else
+    echo -e "${YELLOW}Generating updated documentation...${NC}"
+    yarn docgen
+    
+    git add ../../
+    git commit -m "docs: update api references for v$version"
+    
+    echo -e "${BLUE}Waiting while docs are generated...${NC}"
+    sleep 2
+fi
 
 # Ask about deploying playground app
 read -p "$(echo -e ${YELLOW}Do you want to deploy the playground app? [y/N]: ${NC})" deploy_playground
