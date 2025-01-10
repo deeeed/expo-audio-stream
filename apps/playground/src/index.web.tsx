e7e121dd7763a06d9f6eeb76bb175eada42f8380
abretonc7s <arthur.breton@consensys.net>
2025-01-10 14:23:32 +0800

    feat: redux setup

--- apps/playground/src/index.web.tsx
+++ apps/playground/src/index.web.tsx
@@ -1,27 +1,21 @@
 import '@expo/metro-runtime'
 import { LoadSkiaWeb } from '@shopify/react-native-skia/lib/module/web'
 import { version as SkiaVersion } from 'canvaskit-wasm/package.json'
-import { registerRootComponent } from 'expo'
 import { renderRootComponent } from 'expo-router/build/renderRootComponent'
-import { Platform } from 'react-native'
 
 import { AppRoot } from './AppRoot'
 
-if (Platform.OS === 'web') {
-    LoadSkiaWeb({
-        locateFile: (path) => {
-            const url = `https://cdn.jsdelivr.net/npm/canvaskit-wasm@${SkiaVersion}/bin/full/${path}`
-            console.log(`__DEV__=${__DEV__} Loading Skia: ${url}`)
-            return url
-        },
+LoadSkiaWeb({
+    locateFile: (path) => {
+        const url = `https://cdn.jsdelivr.net/npm/canvaskit-wasm@${SkiaVersion}/bin/full/${path}`
+        console.log(`__DEV__=${__DEV__} Loading Skia: ${url}`)
+        return url
+    },
+})
+    .then(async () => {
+        renderRootComponent(AppRoot)
+        return true
     })
-        .then(async () => {
-            renderRootComponent(AppRoot)
-            return true
-        })
-        .catch((error) => {
-            console.error('Failed to load Skia', error)
-        })
-} else {
-    registerRootComponent(AppRoot)
-}
+    .catch((error) => {
+        console.error('Failed to load Skia', error)
+    })
