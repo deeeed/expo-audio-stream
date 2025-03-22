9b58e90c9016fc3e6cc60cf41d3030046afc366c
abretonc7s <arthur.breton@consensys.net>
2025-03-22 18:12:35 +0100

    chore: deployment process for essentia

---
+++ packages/react-native-essentia/publisher.config.ts
@@ -0,0 +1,39 @@
+import type { ReleaseConfig, DeepPartial } from '@siteed/publisher';
+
+const config: DeepPartial<ReleaseConfig> = {
+  packageManager: 'yarn',
+  changelogFile: 'CHANGELOG.md',
+  conventionalCommits: true,
+  changelogFormat: 'conventional',
+  versionStrategy: 'independent',
+  bumpStrategy: 'prompt',
+  packValidation: {
+    enabled: false,
+    validateFiles: true,
+    validateBuildArtifacts: true,
+  },
+  git: {
+    tagPrefix: '',
+    requireCleanWorkingDirectory: true,
+    requireUpToDate: true,
+    requireUpstreamTracking: true,
+    commit: true,
+    push: true,
+    commitMessage:
+      'chore(react-native-essentia): release @siteed/react-native-essentia@${version}',
+    tag: true,
+    allowedBranches: ['main', 'master'],
+    remote: 'origin',
+  },
+  npm: {
+    publish: true,
+    registry: 'https://registry.npmjs.org',
+    tag: 'latest',
+    access: 'public',
+  },
+  hooks: {},
+  updateDependenciesOnRelease: false,
+  dependencyUpdateStrategy: 'none',
+};
+
+export default config;
